df58a1028f0a02e57a449c71a5641581fcaec5ff
Nicolas Le Breton <kengo14@hotmail.fr>
2015-03-17 21:47:01 +0100

    Debut du scription move red pion

---
+++ Assets/Scripts/MovePionRed.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePionRed : MonoBehaviour {
+
+	public Rigidbody Possibility;
+
+	void OnMouseDown ()
+	{
+		Debug.Log("Hello", gameObject);
+		Rigidbody clone;
+		clone = Instantiate(Possibility, transform.position, transform.rotation) as Rigidbody;
+		clone.velocity = transform.TransformDirection(Vector3.forward * 10);
+	}
+}
